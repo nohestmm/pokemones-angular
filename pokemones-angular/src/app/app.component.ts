72605530f8b45b68f83582de79cb10e6a5db7336
LaboratoriaD163 <inapadfoot@yahoo.com>
2019-10-30 22:36:46 -0300

    adding components to app

--- pokemones-angular/src/app/app.component.ts
+++ pokemones-angular/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'poke-root',//<poke-root></poke-root>
-  template: `
-  <app-navbar></app-navbar>
-  <app-sidebar></app-sidebar>
-  <app-dashboard></app-dashboard>
-  `,
+  styleUrls:['app.component.css'],
+
+template:`
+<app-filter></app-filter>
+<app-dashboard></app-dashboard>
+<app-details></app-details>
+`
+
+
+
  
 })
 export class AppComponent {
